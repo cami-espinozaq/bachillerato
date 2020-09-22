4be08f00ff7956a264ad21685526b0a593c4854a
Camila Espinoza <camila.espinoza@factor-a.de>
2020-09-22 22:53:10 +0200

    music category

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import classes from './Categories.module.css';
 import { ReactComponent as Parrot } from '../assets/images/parrot-opt-group.svg';
+import { ReactComponent as Music } from '../assets/images/music-neon-opt.svg';
 import { Grid, Typography } from '@material-ui/core';
 
 import Wrapper from '../UI/Wrapper';
 
 const Category = props => {
 
-    const style = `${classes.block} ${props.color}`
+    const colorClass = {
+        'cyan': classes.cyan,
+        'purple': classes.purple,
+        'fuccia': classes.fuccia,
+        'orange': classes.orange
+    }
+    const image = {
+        'animals': <Parrot />,
+        'music': <Music />
+    }
+    const style = `${classes.block} ${colorClass[props.color]}`;
 
     return (
         <div className={style}>
-            <Typography variant="h4">Animals</Typography>
-            <Parrot />
+            <Typography variant="h4">{props.name}</Typography>
+            {image[props.name.toLowerCase()]}
         </div>
     )
 };
 
 const categories = props => {
+
+    const colors = ['cyan', 'purple', 'fuccia', 'orange'];
+    const categoryList = ['Animals', 'Music'];
+
+    let categories = categoryList.map((name, index) => {
+        const colorIndex = index % 4;
+        return <Category color={colors[colorIndex]} name={name} />
+    });
+
     return (
         <Wrapper logoLink="/settings">
             <Grid container spacing={6}>
                 <Grid item xs={12} md={4}>
-                    <Category color={classes.blockCyan}></Category>
+                    {categories}
                 </Grid>
             </Grid>
         </Wrapper>
