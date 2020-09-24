ae665d2c7269cd906f03a9e7e65ba449bc887b0d
Camila Espinoza <camila.espinoza@factor-a.de>
2020-09-24 23:51:29 +0200

    more categories

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -2,6 +2,9 @@ import React from 'react';
 import classes from './Categories.module.css';
 import { ReactComponent as Parrot } from '../assets/images/parrot-opt-group.svg';
 import { ReactComponent as Music } from '../assets/images/music-neon-opt.svg';
+import { ReactComponent as Fruit } from '../assets/images/pineapple-opt.svg';
+import { ReactComponent as World } from '../assets/images/world1-opt.svg';
+import { ReactComponent as People } from '../assets/images/avatar-opt.svg';
 import { Grid, Typography } from '@material-ui/core';
 
 import Wrapper from '../UI/Wrapper';
@@ -16,7 +19,10 @@ const Category = props => {
     }
     const image = {
         'animals': <Parrot />,
-        'music': <Music />
+        'music': <Music />,
+        'fruits': <Fruit />,
+        'countries': <World />,
+        'celebrities': <People />
     }
     const style = `${classes.block} ${colorClass[props.color]}`;
 
@@ -31,7 +37,7 @@ const Category = props => {
 const categories = props => {
 
     const colors = ['cyan', 'purple', 'fuccia', 'orange'];
-    const categoryList = ['Animals', 'Music'];
+    const categoryList = ['Animals', 'Music', 'Fruits', 'Countries', 'Celebrities'];
 
     let categories = categoryList.map((name, index) => {
         const colorIndex = index % 4;
